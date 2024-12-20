d6515a35a74b372ce4f93d115cc3e8ddc616e1eb
Dhikshanya <115531106+dhikshanya06@users.noreply.github.com>
2024-12-20 18:09:00 +0530

    Delete lab 1 directory

--- lab 1/greeting.hs
+++
@@ -1,5 +0,0 @@
-main :: IO ()
-main = do
-    putStrLn "What is your name?"
-    name <- getLine
-    putStrLn ("Hello, " ++ name)
--- lab 1/isEmpty.hs
+++
@@ -1,8 +0,0 @@
-isEmpty :: [a] -> Bool
-isEmpty [] = True
-isEmpty _  = False
-
-main :: IO ()
-main = do
-    print (isEmpty [1, 2, 3])  
-    print (isEmpty [])         
